23cd51e6e1498a5ca683f07b8eed539cc79a8abf
Cristofer <cristofer_flort@hotmail.com>
2016-09-06 19:15:37 -0500

    Creada interfaz de configuracion
    Se creo la interfaz de configuracion para el usuario no-administrador

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-
+(function(){
+	  	// show number case add shoes.
+	if($('#category-chooser').val() === '1'){
+		$('#shoes-pick').removeClass('hidden');
+		$('#save-btn').removeClass('mt');
+	}else{
+		$('#shoes-pick').addClass('hidden');
+		$('#save-btn').addClass('mt');		
+	}
+	$('#category-chooser').change(function(){
+		if($(this).val() === '1'){
+			$('#shoes-pick').removeClass('hidden');
+			$('#save-btn').removeClass('mt');
+		}
+		else{
+			$('#shoes-pick').addClass('hidden');
+			$('#save-btn').addClass('mt');
+		}
+	});
+}());
